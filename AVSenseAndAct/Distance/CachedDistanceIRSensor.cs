913caa4a4ca8c44dfe717f52e43a54a904cae7a3
Jan Sandbrink <fischikowski@web.de>
2013-01-29 23:24:49 +0100

    add a cached version of the distanceIR sensor using callbacks

---
+++ AVSenseAndAct/Distance/CachedDistanceIRSensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tinkerforge;
+
+namespace AutonomousVehicle.SenseAndAct.Distance
+{
+    public class CachedDistanceIRSensor : IDistanceSensor
+    {
+        public Distance Distance { get; private set; }
+
+        public CachedDistanceIRSensor(BrickletDistanceIR bricklet)
+        {
+            bricklet.Distance += UpdateDistance;
+        }
+
+        private void UpdateDistance(BrickletDistanceIR sender, int distance)
+        {
+            Distance = Distance.FromMillimeters(distance);
+        }
+    }
+}
